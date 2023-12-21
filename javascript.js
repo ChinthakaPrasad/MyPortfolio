c1a7457122a9b66a224819cb397fb99a2ad54de2
Chinthaka Prasad <chinthapc@gmail.com>
2023-12-22 00:42:53 +0530

    js file added with few functionalities

--- javascript.js
+++ javascript.js
@@ -1,14 +1,40 @@
 var contactMeBtn = document.getElementById("contactMeBtn");
 
-contactMeBtn.addEventListener('click', ()=>{
+contactMeBtn.addEventListener(
+  "click",
+  () => {
     window.location.href = "mailto:chinthakaprasad30@gmail.com";
+  },
+  "_blank"
+);
 
-}, _blank);
+let viewCvBtn = document.getElementById("viewCv");
 
-var viewCvBtn = document.getElementById("viewCv");
-
-viewCvBtn.addEventListener('click', ()=>{
-    const pdfUrl = 'src/ICM 104066 chinthaka prasad.pdf';
-    window.open(pdfUrl, '_blank');
-
+viewCvBtn.addEventListener("click", () => {
+  const pdfUrl = "src/js.png";
+  window.open(pdfUrl, "_blank");
 });
+
+document.getElementById("email").onclick = () => {
+  window.open("mailto:chinthakaprasad30@gmail.com", "_blank");
+};
+
+document.getElementById("gitHub").onclick = () => {
+  window.open("https://github.com/ChinthakaPrasad?tab=repositories", "_blank");
+};
+
+document.getElementById("stackOverFlow").onclick = () => {
+  window.open(
+    "https://stackoverflow.com/users/22200258/chinthaka-prasad-wijerathna",
+    "_blank"
+  );
+};
+
+document.getElementById("linkdin").onclick = () => {
+  window.open(
+    "https://www.linkedin.com/in/chinthaka-prasad-wijerathna-85b354167/",
+    "_blank"
+  );
+};
+
+
